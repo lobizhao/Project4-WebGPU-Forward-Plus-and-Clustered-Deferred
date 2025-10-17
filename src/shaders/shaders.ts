233903a9a477cbde1a33e6bf02eb8d9f3f9bf2dc
lobizhao <lobizhao@gmail.com>
2025-10-17 02:51:36 -0400

    withdraw

--- src/shaders/shaders.ts
+++ src/shaders/shaders.ts
@@ -40,12 +40,7 @@ export const constants = {
     tileCountY: 9,
     tileCountZ: 24,
     maxLightsPerCluster: 2048
-} as const;
-
-// Prevent minification from removing unused constants
-if (false) {
-    console.log(constants.lightRadius, constants.tileCountZ, constants.maxLightsPerCluster);
-}
+};
 
 // =================================
 
